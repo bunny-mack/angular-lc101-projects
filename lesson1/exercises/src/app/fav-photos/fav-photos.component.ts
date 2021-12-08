102d00e9f019bc593a0bac6893670f042d8e0217
Bunny McIntyre <bmcintyre.133@gmail.com>
2021-12-08 13:44:19 -0600

    done

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = "Bunny's Photos";
+  image1 = 'https://i.scdn.co/image/ab6761610000e5eb9e3acf1eaf3b8846e836f441';
+  image2 = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1236509239.jpg?crop=1xw:1xh;center,top&resize=480:*';
+  image3 = 'https://img.buzzfeed.com/buzzfeed-static/static/2020-12/11/16/campaign_images/bae355bd64c3/with-folklore-taylor-swift-has-completed-her-tran-2-3190-1607702418-5_dblbig.jpg';
 
   constructor() { }
 
